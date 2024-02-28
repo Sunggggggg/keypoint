5266d6efe068b6c3a8fedb2d5f66ae2aa8c7a11c
Sunggggggg <sky990319@gmail.com>
2024-02-28 17:03:01 +0900

    f

--- data_lab.py
+++ data_lab.py
@@ -1,10 +1,9 @@
 from dataset import RealEstate10k
 
-img_root = ''
-pose_root =
-num_ctxt_views =
-num_query_views = 
+img_root = '/home/dev4/data/SKY/datasets/data_download/realestate/train'
+pose_root = '/home/dev4/data/SKY/datasets/data_download/poses/realestate/train.mat'
+num_ctxt_views = 2
+num_query_views = 1
 
-RealEstate10k(img_root, pose_root,
-                 num_ctxt_views, num_query_views, query_sparsity=None,
-                 max_num_scenes=None, square_crop=True, augment=True, lpips=False)
+
+train_dataset = RealEstate10k(img_root, pose_root, num_ctxt_views, num_query_views, query_sparsity=192, augment=True, lpips=True)
