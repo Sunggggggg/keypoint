1a2c3d920e4bcc602588204cfc70969039319eaa
Sunggggggg <sky990319@gmail.com>
2024-03-03 16:47:57 +0900

    f

--- models/Render.py
+++ models/Render.py
@@ -7,7 +7,7 @@ from utils import util
 import geometry
 from epipolar import project_rays
 from resnet_block_fc import ResnetFC
-from models.Render import VolumeAttention
+from models.Custom_encoder import VolumeAttention
 from copy import deepcopy
 
 
