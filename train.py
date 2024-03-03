4a9130131592fadde3cf76537f23c339a4387c2b
Sunggggggg <sky990319@gmail.com>
2024-03-03 18:47:55 +0900

    f

--- train.py
+++ train.py
@@ -12,6 +12,9 @@ from dataset import RealEstate10k
 import loss_functions
 import models
 from collections import defaultdict
+import imageio
+
+to8b = lambda x : (255*np.clip(x,0,1)).astype(np.uint8)
 
 def worker_init_fn():
     random.seed(int(torch.utils.data.get_worker_info().seed)%(2**32-1))
@@ -87,101 +90,112 @@ def train(rank, world_size, args):
     total_steps = 0
     
     print("Train start...!")
-    for epoch in range(epochs):
-        print(f"[Epoch] {epoch} Load dataset Train dataset : {len(train_dataset)} Test dataset : {len(val_dataset)}")
-        for step, (model_input, gt) in enumerate(train_dataloader):
-            model_input = util.dict_to_gpu(model_input)
-            gt = util.dict_to_gpu(gt)
+    with tqdm(len(train_dataloader) * epochs) as pbar:
+        for epoch in range(epochs):
+            print(f"[Epoch] {epoch} Load dataset Train dataset : {len(train_dataset)} Test dataset : {len(val_dataset)}")
+            for step, (model_input, gt) in enumerate(train_dataloader):
+                model_input = util.dict_to_gpu(model_input)
+                gt = util.dict_to_gpu(gt)
 
-            model_output = model(model_input)
-            losses, loss_summaries = loss_fn(model_output, gt, model=model)
+                model_output = model(model_input)
+                losses, loss_summaries = loss_fn(model_output, gt, model=model)
 
-            train_loss = 0.
-            for loss_name, loss in losses.items():
-                single_loss = loss.mean()
-                train_loss += single_loss
-            
-            optimizer.zero_grad()
-            train_loss.backward()
+                train_loss = 0.
+                for loss_name, loss in losses.items():
+                    single_loss = loss.mean()
+                    train_loss += single_loss
+                
+                optimizer.zero_grad()
+                train_loss.backward()
 
-            optimizer.step()
-            
-            if rank == 0:
-                total_steps += 1
-                tqdm.write(f"[Epoch: {epoch}] [Iter: {total_steps}] Loss: {train_loss.item():.3f}\t ")
-            del train_loss
+                if world_size > 1:
+                    average_gradients(model)
 
-    #if val_dataloader is not None and total_steps % 100 == 0:
-            print("Running validation set...")
-            with torch.no_grad():
-                model.eval()
-                val_losses = defaultdict(list)
-                for val_i, (model_input, gt) in enumerate(val_dataloader):
-                    print("processing valid")
-                    model_input = util.dict_to_gpu(model_input)
-                    gt = util.dict_to_gpu(gt)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=False)
 
-                    model_input_full = model_input
-                    rgb_full = model_input['query']['rgb']
-                    uv_full = model_input['query']['uv']
-                    nrays = uv_full.size(2)         # 
-                    # chunks = nrays // 512 + 1
-                    chunks = nrays // 512 + 1
-                    # chunks = nrays // 384 + 1
-
-                    z = model.get_z(model_input)
-
-                    rgb_chunks = torch.chunk(rgb_full, chunks, dim=2)
-                    uv_chunks = torch.chunk(uv_full, chunks, dim=2)
-
-                    model_outputs = []
-
-                    for rgb_chunk, uv_chunk in zip(rgb_chunks, uv_chunks):
-                        model_input['query']['rgb'] = rgb_chunk
-                        model_input['query']['uv'] = uv_chunk
-                        model_output = model(model_input, z=z, val=True)
-                        del model_output['z']
-                        del model_output['coords']
-                        del model_output['at_wts']
-
-                        model_output['pixel_val'] = model_output['pixel_val'].cpu()
-
-                        model_outputs.append(model_output)
-
-                    model_output_full = {}
-
-                    for k in model_outputs[0].keys():
-                        outputs = [model_output[k] for model_output in model_outputs]
-
-                        if k == "pixel_val":
-                            val = torch.cat(outputs, dim=-3)
-                        else:
-                            val = torch.cat(outputs, dim=-2)
-                        model_output_full[k] = val
-
-                    model_output = model_output_full
-                    model_input['query']['rgb'] = rgb_full
-
-                    val_loss, val_loss_smry = val_loss_fn(model_output, gt, val=True, model=model)
-
-                    for name, value in val_loss.items():
-                        val_losses[name].append(value)
-
-                    # Render a video
-
-                    # if val_i == batches_per_validation:
-                    break
-
-                for loss_name, loss in val_losses.items():
-                    single_loss = np.mean(np.concatenate([l.reshape(-1).cpu().numpy() for l in loss], axis=0))
-
+                optimizer.step()
+                
                 if rank == 0:
-                    print(model_output_full['rgb'].shape) # [1, 1, HW, 3]
-            model.train()
-    
-        if rank == 0:
-            torch.save({'model': model.state_dict(), 'optimizer': optimizer.state_dict()},
-                    os.path.join(checkpoints_dir, 'model_final.pth'))
+                    pbar.update(1)
+                    total_steps += 1
+                    tqdm.write(f"[Epoch: {epoch}] [Iter: {total_steps}] Loss: {train_loss.item():.3f}\t ")
+                del train_loss
+
+        #if val_dataloader is not None and total_steps % 100 == 0:
+                print("Running validation set...")
+                with torch.no_grad():
+                    model.eval()
+                    val_losses = defaultdict(list)
+                    for val_i, (model_input, gt) in enumerate(val_dataloader):
+                        print("processing valid")
+                        model_input = util.dict_to_gpu(model_input)
+                        gt = util.dict_to_gpu(gt)
+
+                        model_input_full = model_input
+                        rgb_full = model_input['query']['rgb']
+                        uv_full = model_input['query']['uv']
+                        nrays = uv_full.size(2)         # 
+                        # chunks = nrays // 512 + 1
+                        chunks = nrays // 512 + 1
+                        # chunks = nrays // 384 + 1
+                        H, W = rgb_full.shape[-2:]
+
+                        z = model.get_z(model_input)
+
+                        rgb_chunks = torch.chunk(rgb_full, chunks, dim=2)
+                        uv_chunks = torch.chunk(uv_full, chunks, dim=2)
+
+                        model_outputs = []
+
+                        for rgb_chunk, uv_chunk in zip(rgb_chunks, uv_chunks):
+                            model_input['query']['rgb'] = rgb_chunk
+                            model_input['query']['uv'] = uv_chunk
+                            model_output = model(model_input, z=z, val=True)
+                            del model_output['z']
+                            del model_output['coords']
+                            del model_output['at_wts']
+
+                            model_output['pixel_val'] = model_output['pixel_val'].cpu()
+
+                            model_outputs.append(model_output)
+
+                        model_output_full = {}
+
+                        for k in model_outputs[0].keys():
+                            outputs = [model_output[k] for model_output in model_outputs]
+
+                            if k == "pixel_val":
+                                val = torch.cat(outputs, dim=-3)
+                            else:
+                                val = torch.cat(outputs, dim=-2)
+                            model_output_full[k] = val
+
+                        model_output = model_output_full
+                        model_input['query']['rgb'] = rgb_full
+
+                        val_loss, val_loss_smry = val_loss_fn(model_output, gt, val=True, model=model)
+
+                        for name, value in val_loss.items():
+                            val_losses[name].append(value)
+
+                        # Render a video
+
+                        # if val_i == batches_per_validation:
+                        break
+
+                    for loss_name, loss in val_losses.items():
+                        single_loss = np.mean(np.concatenate([l.reshape(-1).cpu().numpy() for l in loss], axis=0))
+
+                    if rank == 0:
+                        rgb = model_output_full['rgb'].reshape(1, 1, H, W, 3) # [1, 1, HW, 3]
+                        rgb8 = to8b(rgb[0][0].cpu().numpy())
+                        filename = os.path.join(output_dir, args.expname, '{:03d}_{:06:d}.png'.format(epoch, total_steps))
+                        imageio.imwrite(filename, rgb8)
+                model.train()
+        
+            if rank == 0:
+                torch.save({'model': model.state_dict(), 'optimizer': optimizer.state_dict()},
+                        os.path.join(checkpoints_dir, 'model_final.pth'))
             
 if __name__ == "__main__":
     args = config_parser()
